7d2a4c8ed15b930688621c5b7b230c14f41bd0c6
vaaibhavmishra <mishravaibhav12321@gmail.com>
2026-05-25 22:33:10 +0530

    chore: update dep

--- apps/desktop/src/renderer/src/components/ui/spinner.tsx
+++ apps/desktop/src/renderer/src/components/ui/spinner.tsx
@@ -1,9 +1,12 @@
 import { cn } from "@renderer/lib/utils";
-import { Loader2Icon } from "lucide-react";
+import { Loader2 } from "lucide-react";
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({
+  className,
+  ...props
+}: React.ComponentProps<typeof Loader2>) {
   return (
-    <Loader2Icon
+    <Loader2
       role="status"
       aria-label="Loading"
       className={cn("size-4 animate-spin", className)}
